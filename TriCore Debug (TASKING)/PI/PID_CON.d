25d2dac7ee7cdf394d0d6945228ce9684d5ad9ad
WOOJIN <tngkr2010@tukorea.ac.kr>
2025-02-11 10:28:09 +0900

    Remove .gitignore and add all files

---
+++ TriCore Debug (TASKING)/PI/PID_CON.d
@@ -0,0 +1,18 @@
+PID_CON.o :	../PI/PID_CON.c
+../PI/PID_CON.c :
+PID_CON.o :	..\PI\PID_CON.h
+..\PI\PID_CON.h :
+PID_CON.o :	"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\iLLD\\TC27D\\Tricore\\Cpu\\Std\Ifx_Types.h"
+"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\iLLD\\TC27D\\Tricore\\Cpu\\Std\Ifx_Types.h" :
+PID_CON.o :	"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h"
+"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\Infra\\Platform\Tricore\Compilers\Compilers.h" :
+PID_CON.o :	"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Configurations\Ifx_Cfg.h"
+"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Configurations\Ifx_Cfg.h" :
+PID_CON.o :	"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h"
+"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\Infra\\Platform\Tricore\Compilers\CompilerTasking.h" :
+PID_CON.o :	"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h"
+"C:\Infineon\AURIX-Studio-1.10.6\tools\Compilers\Tasking_1.1r8\ctc\include\stddef.h" :
+PID_CON.o :	"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\iLLD\\TC27D\\Tricore\\Cpu\\Std\Platform_Types.h"
+"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\iLLD\\TC27D\\Tricore\\Cpu\\Std\Platform_Types.h" :
+PID_CON.o :	"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\iLLD\\TC27D\\Tricore\\Cpu\\Std\Ifx_TypesTasking.h"
+"C:\\Users\\tngkr\\Desktop\\Motor_A_B\\Motor_PI_control\\Libraries\\iLLD\\TC27D\\Tricore\\Cpu\\Std\Ifx_TypesTasking.h" :
